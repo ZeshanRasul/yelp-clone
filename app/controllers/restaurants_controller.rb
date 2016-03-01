b600fe3f1b6cb2d919fbb568a6754e1276196486
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-01 10:48:17 +0000

    Began create new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,13 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    Restaurant.create(params[:restaurant])
+    # redirect_to '/restaurants'
+  end
+
+  # def restaurant_params
+  #   params.require(:restaurant).permit(:name)
+  # end
 end
