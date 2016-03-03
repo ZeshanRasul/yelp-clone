7d186d369806ca771ec2314d1d4e4c9e549929f8
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-03 17:23:35 +0000

    Added average rating tests

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -56,4 +56,18 @@ feature 'reviewing' do
     expect(page).to have_content 'Review deleted'
   end
 
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
+
 end
