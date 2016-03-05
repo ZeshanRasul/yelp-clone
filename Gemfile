d3af80fb1873d7de49a0f800440045bd68cbcb8a
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-05 22:29:03 +0000

    Implemented endorsements and passed tests

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'devise'
 gem 'omniauth-facebook'
 gem 'dotenv-rails'
 
+gem 'paperclip'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,7 +1,3 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
-
 $(document).ready(function(){
 
   $('.endorsements-link').on('click', function(event){
@@ -11,6 +7,6 @@ $(document).ready(function(){
 
     $.post(this.href, function(response){
       endorsementCount.text(response.new_endorsement_count);
-    })
-  })
-})
+    });
+  });
+});
